402156c57ecaa2f6ac59a1560de627f692121ca5
infan <infan@DESKTOP-9II49PR>
2023-05-04 09:28:12 +0530

    demo first project

---
+++ src/main/java/mavan/mavan/App.java
@@ -0,0 +1,13 @@
+package mavan.mavan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
